dc14ebfb667abbd7d10df583762bba04ef12f4fd
수영 김 <ksy9926@naver.com>
2021-07-15 20:39:59 +0900

    #2.4 Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".jsColor");
+const range = document.querySelector("#jsRange");
+const mode = document.querySelector("#jsMode");
 
 canvas.width = 500;
 canvas.height = 500;
@@ -9,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(event) {
   painting = false;
@@ -39,6 +42,21 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -50,3 +68,14 @@ Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
 // Array.from 메소드는 object 로부터 array 를 만든다.
+
+if (range) {
+  // range.addEventListener("change", handleRangeChange);
+  // onChange 의 경우 range를 움직여 나온 최종 상태만 이벤트 발생.
+  // input의 경우 range를 움직이는 매 순간 발생.
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
