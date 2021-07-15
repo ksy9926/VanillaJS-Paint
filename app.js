58dca93fec8469381e95845870b739ea49e58816
수영 김 <ksy9926@naver.com>
2021-07-15 19:38:57 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const canvas = document.querySelector("#jsCanvas");
 let painting = false;
 
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 function stopPainting(event) {
   painting = false;
+}
+
+function startPainting(event) {
+  painting = true;
 }
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
